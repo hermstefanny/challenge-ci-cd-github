e65e71fd81ba6709ffad9f739ee7af4eda9b3b80
Estefania <aida.estefania.sosa@gmail.com>
2025-08-18 16:20:27 +0200

    modified unit test for paths

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -1,12 +1,21 @@
+import os
 from app.RAG_function import get_pdf_paths
 
 
 def test_import_pdfs() -> None:
     """Test for the import function"""
-    correct_pdf_paths = [
-        "C:\\Users\\herms\\Desktop\\BeCode-Bauman\\TheMountain_Projects\\challenge-ci-cd-github\\data\\raw-pdfs\\Acta Sesión Conmemorativa  No. 064 2024-05-24.pdf",
-        "C:\\Users\\herms\\Desktop\\BeCode-Bauman\\TheMountain_Projects\\challenge-ci-cd-github\\data\\raw-pdfs\\Acta Sesión Extraordinaria  No. 052 2024-03-08.pdf",
-        "C:\\Users\\herms\\Desktop\\BeCode-Bauman\\TheMountain_Projects\\challenge-ci-cd-github\\data\\raw-pdfs\\Acta Sesión Ordinaria  No. 056 2024-04-09.pdf",
+    home_directory = os.getcwd()
+    partial_folder_path = "data/raw-pdfs"
+    complete_folder_path = os.path.join(home_directory, partial_folder_path)
+
+    pdf_names = [
+        "Acta Sesión Conmemorativa  No. 064 2024-05-24.pdf",
+        "Acta Sesión Extraordinaria  No. 052 2024-03-08.pdf",
+        "Acta Sesión Ordinaria  No. 056 2024-04-09.pdf",
     ]
 
-    assert get_pdf_paths("data/raw-pdfs") == correct_pdf_paths
+    correct_pdf_paths = [
+        os.path.abspath(os.path.join(complete_folder_path, f)) for f in pdf_names
+    ]
+
+    assert get_pdf_paths(partial_folder_path) == correct_pdf_paths
