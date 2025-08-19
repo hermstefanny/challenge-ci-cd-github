376093c037aba4dc55cad649f9d68938d7ff8622
Estefania <aida.estefania.sosa@gmail.com>
2025-08-19 09:45:29 +0200

    branch qa creation

--- app/main.py
+++ app/main.py
@@ -41,7 +41,7 @@ current_question = st.text_area(
 st.markdown(["this is the test for ci"])
 send_q = st.button("Send", type="primary", use_container_width=True, width="content")
 
-st.write("now in branch dev")
+st.write("now in branch QA")
 if send_q:
     answer = st.write(
         f"You have asked  '{current_question}' about {chosen_pdf} that it's in {pdf_names[chosen_pdf]}"
